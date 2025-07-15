de0c0e8e8f1b4a462b623451df012a417f5fa339
Diketene <liuhaotian0406@163.com>
2025-07-16 00:25:20 +0800

    make definitions more readable

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -7,10 +7,20 @@
 
 namespace wigcpp::internal::def{ 
   /* wigcpp definitions, used by big_int.hpp */
+
+  constexpr inline unsigned sizeof_item = MULTI_WORD_INT_SIZEOF_ITEM;
+
+  constexpr inline unsigned sizeof_mulw = MULTI_WORD_INT_SIZEOF_MULW;
+
+  static_assert(sizeof_item == 4 || sizeof_item == 8, "Invalid MULTI_WORD_INT_SIZEOF_ITEM value, must be 4 or 8");
+
+  static_assert(sizeof_mulw == 4 || sizeof_mulw == 8, "Invalid MULTI_WORD_INT_SIZEOF_MULW value, must be 4 or 8");
+
+  constexpr inline bool has_long_double = WIGCPP_IMPL_LONG_DOUBLE;
+  using double_type = std::conditional_t<has_long_double, long double, double>;
+
   template <unsigned size>
-  struct multi_word_traits{
-    static_assert(size == 4 || size == 8, "Invalid MULTI_WORD_INT_SIZEOF_ITEM value, must be 4 or 8");
-  };
+  struct multi_word_traits{};
 
   template <>
   struct multi_word_traits<8>{
@@ -37,37 +47,29 @@ namespace wigcpp::internal::def{
     using word_t = std::int32_t;     /* signed word type */
     using dword_t = std::int64_t;   	/* signed double word type */
 
-    #if MULTI_WORD_INT_SIZEOF_MULW == 8
-      using u_mul_word_t = std::uint64_t;
-      inline static constexpr bool MWI_MULW_LARGER = true;
-    #elif MULTI_WORD_INT_SIZEOF_MULW == 4
-      using u_mul_word_t = std::uint32_t;
-      inline static constexpr bool MWI_MULW_LARGER = false;
-    #else
-      #error "Invalid MULTI_WORD_INT_SIZEOF_MULW value, must be 4 or 8"
-    #endif
+    using u_mul_word_t = std::conditional_t<sizeof_mulw == 8, std::uint64_t, std::uint32_t>;
+    inline static constexpr bool MWI_MULW_LARGER = (sizeof_mulw == 8);
+
   };
 
-  using Traits = multi_word_traits<MULTI_WORD_INT_SIZEOF_ITEM>;
+  using Traits = multi_word_traits<sizeof_item>;
 
   using uword_t = Traits::uword_t;
   using udword_t = Traits::udword_t;
   using word_t = Traits::word_t;
   using dword_t = Traits::dword_t;
   using u_mul_word_t = Traits::u_mul_word_t;
-  constexpr inline static bool mulw_larger = Traits::MWI_MULW_LARGER;
+  constexpr inline bool mulw_larger = Traits::MWI_MULW_LARGER;
 
-  constexpr inline static bool has_detected_long_double = WIGCPP_IMPL_LONG_DOUBLE;
+  constexpr inline uword_t shift_bits = sizeof(uword_t) << 3;
 
-  using double_type = std::conditional_t<has_detected_long_double, long double, double>;
+  constexpr inline uword_t mulw_shift_bits = sizeof(u_mul_word_t) << 3;
 
-  constexpr inline static uword_t shift_bits = sizeof(uword_t) << 3;
-
-  constexpr inline static uword_t mulw_shift_bits = sizeof(u_mul_word_t) << 3;
+  constexpr inline uword_t sign_bit = ((uword_t)1) << (shift_bits - 1);
 
   template<typename T> 
-  /* only for def::uword_t and def:word_t*/
-  inline constexpr uword_t full_sign_word(T x) {
+  /* only for def::uword_t and def:word_t */
+  constexpr inline uword_t full_sign_word(T x) {
     if constexpr (std::is_signed_v<T>) {
       return static_cast<uword_t>(x >> (shift_bits - 1));
     } else {
@@ -75,7 +77,6 @@ namespace wigcpp::internal::def{
     }
   }
 
-  inline constexpr uword_t sign_bit = ((uword_t)1) << (shift_bits - 1);
 };
 
 #endif /* __WIGCPP_DEFINITIONS__*/
