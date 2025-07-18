b5b9aca3bdc10ec6df8111d292d5b270472f24ad
Diketene <liuhaotian0406@163.com>
2025-07-18 09:52:40 +0800

    remove method nothrow_allocator::reallocate

--- src/nothrow_allocator.hpp
+++ src/nothrow_allocator.hpp
@@ -1,10 +1,10 @@
-#ifndef __WIGCPP_NOTHROW_ALLOC
-#define __WIGCPP_NOTHROW_ALLOC
+#ifndef __WIGCPP_NOTHROW_ALLOCATOR__
+#define __WIGCPP_NOTHROW_ALLOCATOR__
 #include <cstddef>
 #include <cstdlib>
-#include <type_traits>
-namespace wigcpp::internal::mwi{
-  template <typename T> // only for POD types
+#include <new>
+namespace wigcpp::internal::allocator{
+  template <typename T> 
   class nothrow_allocator{
     public:
       using value_type = T;
@@ -13,21 +13,29 @@ namespace wigcpp::internal::mwi{
       template<typename U>
       nothrow_allocator(const nothrow_allocator<U> &alloc) noexcept {};
 
-      [[nodiscard]]T* allocate(std::size_t n) noexcept{
-        return static_cast<T*>(std::malloc(n *sizeof(T)));
+      [[nodiscard]]value_type* allocate(std::size_t n) noexcept{
+        if(n == 0) return nullptr;
+        constexpr std::size_t alignment = alignof(value_type);
+        const std::size_t size = n * sizeof(value_type);
+        if constexpr(alignment > alignof(std::max_align_t)){
+          return static_cast<value_type*>(::operator new(size, std::align_val_t{alignment}, std::nothrow));
+        }
+        return static_cast<value_type*>(std::malloc(size));
       }
 
-      T* reallocate(T *p, std::size_t new_size)noexcept{
-        static_assert(std::is_trivial_v<T>, "Only trivial types can be reallocated.\n");
-        return static_cast<T*>(std::realloc(p, new_size * sizeof(T)));
-      }
-
-      void deallocate(T* p, std::size_t n) noexcept{
-        (void)n; //unused
-        std::free(p);
+      void deallocate(value_type* p, std::size_t n) noexcept{
+        if(!p || !n) return;
+        constexpr std::size_t alignment = alignof(value_type);
+        const std::size_t size = sizeof(value_type) * n;
+        if constexpr(alignment > alignof(std::max_align_t)){
+          ::operator delete(static_cast<void*>(p), size, std::align_val_t{alignment});
+          return;
+        }else{
+          std::free(static_cast<void*>(p));
+          return;
+        }
       }
   };
 
-
 }
-#endif
+#endif /* __WIGCPP_NOTHROW_ALLOCATOR__ */
