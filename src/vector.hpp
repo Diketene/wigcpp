d53561c2f44b4c6290106843f03b75b8e62d8d0b
Diketene <liuhaotian0406@163.com>
2025-07-23 16:37:27 +0800

    constructed a nothrow vector for nothrow types

---
+++ src/vector.hpp
@@ -0,0 +1,168 @@
+#ifndef __WIGCPP_POD_VECTOR__
+#define __WIGCPP_POD_VECTOR__
+
+#include "nothrow_allocator.hpp"
+#include "error.hpp"
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+namespace wigcpp::internal::container{
+
+  template <typename T, class Allocator = allocator::nothrow_allocator<T>>
+  class vector{
+    using alloc_traits = std::allocator_traits<Allocator>;
+    using value_type = T;
+    using size_type = std::size_t;
+    static Allocator allocator;
+    
+    value_type* data;
+    value_type* first_free;
+    value_type* cap;
+    
+    [[nodiscard]] value_type *alloc(size_type capacity) noexcept {
+      value_type p = alloc_traits::allocate(allocator, capacity);
+      if(!p){
+        std::fprintf(stderr, "error in wigcpp::internal::container::vector::allocate: allocation failed.\n");
+        error::error_process(error::ErrorCode::Bad_Alloc);
+      }
+      return p;
+    }
+
+    template <typename ...Args>
+    void construct_at(value_type *dest, Args &&...args) noexcept {
+      alloc_traits::construct(allocator, dest, std::forward<Args>(args)...);
+    }
+
+    void destory_at(value_type *dest) noexcept {
+      alloc_traits::destory(allocator, dest);
+    }
+
+    void destory_elements() noexcept {
+      for(value_type* it = first_free ; it != data; ){
+        --it;
+        destory_at(it);
+      }
+    }
+
+    void release_memory() noexcept {
+      alloc_traits::deallocate(allocator, data, capacity());
+    }
+
+    void free() noexcept{
+      if constexpr(!std::is_trivially_destructible_v<value_type>){
+        destory_elements();
+      }
+        release_memory();
+    }
+
+  public:
+    vector() noexcept: data(nullptr), first_free(nullptr), cap(nullptr){}
+
+    vector(const vector &src) noexcept {
+      static_assert(std::is_nothrow_copy_constructible_v<value_type>, "value_type T must have nothrow copy constructor.");
+      value_type *new_data = allocate(src.capacity());
+      data = new_data;
+      first_free = new_data + src.size();
+      cap = new_data + src.capacity();
+
+      if constexpr(!std::is_trivially_copyable_v<value_type>){
+        value_type *src_element = src.data;
+        for(value_type *it = data; it != first_free; ++it){
+          construct_at(it, *src_element);
+          ++src_element; 
+        }
+      }else{
+        std::memcpy(data, src.data, src.size() * sizeof(value_type));
+      }
+    }
+
+    vector(vector &&src) noexcept :data(src.data), first_free(src.first_free), cap(src.cap){
+      src.data = src.first_free = src.cap = nullptr;
+    }
+
+    vector &operator= (const vector &src) noexcept {
+      if(this == &src){
+        return *this;
+      }
+      value_type *new_data = alloc(src.capacity());
+      free();
+
+      data = new_data;
+      first_free = new_data + src.size();
+      cap = new_data + src.capacity();
+
+      if constexpr(!std::is_trivially_copyable_v<value_type>){
+        value_type *src_elem = src.data;
+        for(value_type *it = data; it != first_free; ++it){
+          construct_at(it, *src_elem);
+          ++src_elem;
+        }
+      }else{
+        std::memcpy(data, src.data, src.size() * sizeof(value_type));
+      }
+
+      return *this;
+    }
+
+    vector &operator=(vector &&src) noexcept {
+      if(*this == src){
+        return *this;
+      }
+      free();
+      data = src.data;
+      first_free = src.first_free;
+      cap = src.cap;
+      src.data = src.first_free = src.cap = nullptr;
+      return *this;
+    }
+
+    template <typename ...Args>
+    vector(size_type size, Args &&... args) noexcept {
+      static_assert(sizeof...(Args) == 0 ? 
+                    std::is_nothrow_default_constructible_v<value_type>: 
+                    std::is_nothrow_constructible_v<value_type, Args...>, "value_type T must have nothrow constructor under given parameters");
+
+      value_type *new_data = alloc(size);
+
+      data = new_data;
+      first_free = new_data + size;
+      cap = new_data + size;  /* doesn't have free capacity after first_free */
+
+      if constexpr(!std::is_trivially_constructible_v<value_type>){
+        for(value_type *it = data; it != first_free; ++it){
+          construct_at(it, std::forward<Args>(args)...);
+        }
+      }else{
+        if constexpr(sizeof...(Args) == 0){
+          std::memset(data, size * sizeof(value_type), 0);
+        }else if constexpr(sizeof...(Args) == 1){
+          const auto val = std::get<0>(std::make_tuple(args...));
+          for(value_type *it = data; it != first_free; ++it){
+            *it = val; 
+          }
+        }else{
+          static_assert(sizeof...(Args) <= 1, "constructor for trival types must have less than 2 parameters");
+        }
+      }
+    }
+    
+    ~vector()noexcept{
+      static_assert(std::is_nothrow_destructible_v<value_type>, "value_type T must have nothrow destructor");
+      free();
+    }
+
+    size_type size() const noexcept {
+      return first_free - data;
+    }
+
+    size_type capacity() const noexcept {
+      return cap - data;
+    }
+  };
+}
+#endif
