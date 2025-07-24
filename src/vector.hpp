606352234beed31641e7043661b98c7d6ac4129e
Diketene <liuhaotian0406@163.com>
2025-07-24 23:56:59 +0800

    add emplace_back and push_back methods for vector

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,13 +1,13 @@
-#ifndef __WIGCPP_POD_VECTOR__
-#define __WIGCPP_POD_VECTOR__
+#ifndef __WIGCPP_VECTOR__
+#define __WIGCPP_VECTOR__
 
 #include "nothrow_allocator.hpp"
 #include "error.hpp"
+#include "templates.hpp"
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
 #include <memory>
-#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -27,7 +27,7 @@ namespace wigcpp::internal::container{
     [[nodiscard]] value_type *alloc(size_type capacity) noexcept {
       value_type *p = alloc_traits::allocate(allocator, capacity);
       if(!p){
-        std::fprintf(stderr, "error in wigcpp::internal::container::vector::allocate: allocation failed.\n");
+        std::fprintf(stderr, "error in wigcpp::internal::container::vector::alloc: allocation failed.\n");
         error::error_process(error::ErrorCode::Bad_Alloc);
       }
       return p;
@@ -110,7 +110,7 @@ namespace wigcpp::internal::container{
     }
 
     vector &operator=(vector &&src) noexcept {
-      if(*this == src){
+      if(this == &src){
         return *this;
       }
       free();
@@ -141,7 +141,7 @@ namespace wigcpp::internal::container{
         if constexpr(sizeof...(Args) == 0){
           std::memset(data, 0, sizeof(value_type) * size);
         }else if constexpr(sizeof...(Args) == 1){
-          const auto val = std::get<0>(std::make_tuple(args...));
+          const auto&& val = (std::forward<Args>(args), ...);
           for(value_type *it = data; it != first_free; ++it){
             *it = val; 
           }
@@ -171,9 +171,58 @@ namespace wigcpp::internal::container{
     const value_type &operator[](size_type index) const{
       return *(data + index);
     }
+
+    void reserve(size_type min_capacity) noexcept{
+      const size_type oldsize = size();
+      const size_type oldcap = capacity();
+      if(min_capacity <= oldcap){
+        return;
+      }
+      const size_type double_cap = capacity() * 2;
+      const size_type new_capacity = min_capacity > double_cap ? min_capacity : double_cap;
+      value_type *new_data = alloc(new_capacity);
+      if constexpr(!std::is_trivially_copyable_v<value_type>){
+        value_type *new_elem = new_data;
+        for(value_type *it = data; it != first_free; ++it){
+          construct_at(new_elem, *it);
+          ++new_elem;
+        }
+      }else{
+        std::memcpy(new_data, data, oldsize * sizeof(value_type));
+      }
+      free();
+      data = new_data;
+      first_free = new_data + oldsize;
+      cap = new_data + new_capacity;
+    }
+
+    template <typename ...Args>
+    void emplace_back(Args &&...args){
+      const size_type szof = size();
+      const size_type capof = capacity();
+      if(szof == capof){
+        reserve(capof + 1);
+      }
+      if constexpr(sizeof...(Args) == 1){
+        using Type = templates::first_type_t<Args...>;
+        if constexpr(std::is_trivially_copyable_v<value_type> && std::is_same_v<value_type, Type>){
+          *first_free = (std::forward<Args>(args), ...);
+        }else{
+          construct_at(first_free, std::forward<Args>(args)...); 
+        }
+      }else{
+        construct_at(first_free, std::forward<Args>(args)...);
+      }
+      ++first_free;
+    }
+
+    template <typename Arg, std::enable_if<std::is_same_v<std::decay_t<Arg>, value_type>>>
+    void push_back(Arg &&arg){
+      emplace_back(std::forward<Arg>(arg));
+    }
   };
 
   template <typename T, class Allocator>
   inline Allocator vector<T, Allocator>::allocator;
 }
-#endif
+#endif /* __WIGCPP_VECTOR__ */
