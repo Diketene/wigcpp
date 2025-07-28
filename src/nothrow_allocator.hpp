42a239aeebbc1391317c5954ddc2eb25a3c432e3
Diketene <liuhaotian0406@163.com>
2025-07-28 13:26:21 +0800

    add template parameter std::size_t to make nothrow_allocator support alignment more than alignof(T)

--- src/nothrow_allocator.hpp
+++ src/nothrow_allocator.hpp
@@ -4,8 +4,11 @@
 #include <cstdlib>
 #include <new>
 namespace wigcpp::internal::allocator{
-  template <typename T> 
+  template <typename T, std::size_t Alignment = alignof(T)> 
   class nothrow_allocator{
+    static_assert(Alignment > 0, "Alignment must be greater than 0");
+    static_assert((Alignment & (Alignment - 1)) == 0, "Alignment must be a power of two");
+    static_assert(Alignment >= alignof(T), "Alignment must be greater than or equal to alignof(T)");
     public:
       using value_type = T;
       nothrow_allocator() noexcept = default;
@@ -15,20 +18,29 @@ namespace wigcpp::internal::allocator{
 
       [[nodiscard]]value_type* allocate(std::size_t n) noexcept{
         if(n == 0) return nullptr;
-        constexpr std::size_t alignment = alignof(value_type);
+
+        constexpr std::size_t alignof_T = alignof(value_type);
         const std::size_t size = n * sizeof(value_type);
-        if constexpr(alignment > alignof(std::max_align_t)){
-          return static_cast<value_type*>(::operator new(size, std::align_val_t{alignment}, std::nothrow));
+
+        if constexpr(Alignment > alignof_T){
+          return static_cast<value_type*>(::operator new(size, std::align_val_t{Alignment}, std::nothrow));
+        }else if constexpr(alignof_T > alignof(std::max_align_t)){
+          return static_cast<value_type*>(::operator new(size, std::align_val_t{alignof_T}, std::nothrow));
+        }else{
+          return static_cast<value_type*>(std::malloc(size));
         }
-        return static_cast<value_type*>(std::malloc(size));
       }
 
       void deallocate(value_type* p, std::size_t n) noexcept{
         if(!p || !n) return;
-        constexpr std::size_t alignment = alignof(value_type);
+        constexpr std::size_t alignof_T = alignof(value_type);
         const std::size_t size = sizeof(value_type) * n;
-        if constexpr(alignment > alignof(std::max_align_t)){
-          ::operator delete(static_cast<void*>(p), size, std::align_val_t{alignment});
+
+        if constexpr(Alignment > alignof_T){
+          ::operator delete(static_cast<void*>(p), size, std::align_val_t{Alignment});
+          return;
+        }else if constexpr(alignof_T > alignof(std::max_align_t)){
+          ::operator delete(static_cast<void*>(p), size, std::align_val_t{alignof_T});
           return;
         }else{
           std::free(static_cast<void*>(p));
