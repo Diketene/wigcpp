5e679a3e5ff4caa60c246bd446cfa430b47f8d04
Diketene <liuhaotian0406@163.com>
2025-07-30 10:33:35 +0800

    update tests

--- test/test_prime_factor.cpp
+++ test/test_prime_factor.cpp
@@ -2,7 +2,7 @@
 #include "factor_pool.hpp"
 TEST(test_prime_factor, test_prime_table) {
   using namespace wigcpp::internal::global;
-  const auto &pool = get_global_factorial_pool(1000);
+  const auto &pool = factorial_pool_init(1000);
   pool.print_factorial_pool();
   pool.print_factorial_used();
 }
