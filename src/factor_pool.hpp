a8e2139ed44dbd90d690763cbd56aa3e26b14767
Diketene <liuhaotian0406@163.com>
2025-08-01 17:04:45 +0800

    change the usage of GlobalFactorPool

--- src/factor_pool.hpp
+++ src/factor_pool.hpp
@@ -3,12 +3,15 @@
 
 #include "definitions.hpp"
 #include "nothrow_allocator.hpp"
+#include "big_int.hpp"
 #include "vector.hpp"
 #include "error.hpp"
 #include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
+#include <cstring>
 
 #ifdef DEBUG_PRINT
 #include <iostream>
@@ -21,11 +24,12 @@ namespace wigcpp::internal::global {
     template <typename T>
     using vector = container::vector<T>;
 
-    vector<char> is_prime;
     vector<uint32_t> prime_list;
     uint32_t num_primes;
+    int max_factorial;
 
-    PrimeTable(int max_factorial) noexcept : is_prime(max_factorial + 1, true), prime_list{}, num_primes{0} {
+    PrimeTable(int max_factorial) noexcept : prime_list{}, num_primes{0}, max_factorial(max_factorial){
+      vector<char> is_prime(max_factorial + 1, true);
       for(int i = 2; i * i < max_factorial; ++i){
         if(is_prime[i]){
           for(int j = i * i; j <= max_factorial; j += i){
@@ -63,6 +67,145 @@ namespace wigcpp::internal::global {
     const exp_t &operator[](std::size_t i) const noexcept {
       return data()[i];
     }
+
+    void set_zero() noexcept {
+      std::memset(data(), 0, sizeof(exp_t) * block_used);
+    }
+
+    /* for functions contain parameter num_blocks, 
+    ** it's caller's duty to make sure that num_blocks won't greater than block size. 
+    */
+
+    void expand_blocks(int num_blocks) noexcept {
+      if(block_used >= num_blocks) return;
+      block_used = num_blocks;
+    } 
+
+    void set_max(int num_blocks) noexcept {
+      std::fill(data(), data() + num_blocks, def::prime::max_exp);
+    }
+
+    void keep_min(const prime_exponents_view &other) noexcept {
+      assert(this -> block_used == other.block_used);
+      for(int i = 0; i < block_used; ++i){
+        data()[i] = std::min(data()[i], other.data()[i]);
+      }
+    }
+
+    void keep_min_in_as_diff(prime_exponents_view &other) noexcept {
+      assert(this -> block_used == other.block_used);
+      for(int i = 0; i < block_used; ++i){
+        exp_t tmp = other.data()[i] - data()[i];
+        data()[i] = std::min(data()[i], other.data()[i]);
+        other.data()[i] = tmp;
+      }
+    }
+
+    void copy(const prime_exponents_view &other) noexcept {
+      this -> block_used = other.block_used;
+      std::copy(other.data(), other.data() + block_used, data());
+    }
+
+    void expand_add(const prime_exponents_view &other) noexcept {
+      expand_blocks(other.block_used);
+      for(int i = 0; i < block_used; ++i){
+        data()[i] += other.data()[i];
+      }
+    }
+
+    void expand_sub(const prime_exponents_view &other) noexcept{
+      expand_blocks(other.block_used);
+      for(int i = 0; i < block_used; ++i){
+        data()[i] -= other.data()[i];
+      }
+    }
+
+    void expand_sum3(prime_exponents_view &a,
+                     prime_exponents_view &b,
+                     prime_exponents_view &c) noexcept {
+      const int max_blocks = std::max({a.block_used, b.block_used, c.block_used});
+      expand_blocks(max_blocks);
+      a.expand_blocks(max_blocks);
+      b.expand_blocks(max_blocks);
+      c.expand_blocks(max_blocks);
+      for(int i = 0; i < max_blocks; ++i){
+        data()[i] = a.data()[i] + b.data()[i] + c.data()[i];
+      }
+    }
+
+    void add3_sub(const prime_exponents_view &a,
+                  const prime_exponents_view &b,
+                  const prime_exponents_view &c,
+                  const prime_exponents_view &d) noexcept {
+      for(int i = 0 ; i < block_used; ++i){
+        data()[i] += a.data()[i] + b.data()[i] + c.data()[i] - d.data()[i];
+      }
+    }
+
+    void add6(const prime_exponents_view &a,
+              const prime_exponents_view &b,
+              const prime_exponents_view &c,
+              const prime_exponents_view &d,
+              const prime_exponents_view &e,
+              const prime_exponents_view &f) noexcept {
+      for(int i = 0; i < block_used; ++i){
+        data()[i] += a.data()[i] + b.data()[i] + c.data()[i] + d.data()[i] + e.data()[i] + f.data()[i];
+      }
+    }
+
+    void add_sub3(const prime_exponents_view &a,
+                     const prime_exponents_view &b,
+                     const prime_exponents_view &c,
+                     const prime_exponents_view &d) noexcept {
+      for(int i = 0; i < block_used; ++i){
+        data()[i] += a.data()[i] - b.data()[i] - c.data()[i] - d.data()[i];
+      }
+    }
+
+    void sum_sub7(const prime_exponents_view &a,
+                  const prime_exponents_view &b,
+                  const prime_exponents_view &c,
+                  const prime_exponents_view &d,
+                  const prime_exponents_view &e,
+                  const prime_exponents_view &f,
+                  const prime_exponents_view &g,
+                  const prime_exponents_view &h, int num_blocks) noexcept {
+      block_used = num_blocks;
+      for(int i = 0; i < block_used; ++i){
+        data()[i] = a.data()[i] - b.data()[i] - c.data()[i] - d.data()[i] - e.data()[i] - f.data()[i] - g.data()[i];
+      }
+    }
+
+    void sum0_sub6(const prime_exponents_view &a,
+                  const prime_exponents_view &b,
+                  const prime_exponents_view &c,
+                  const prime_exponents_view &d,
+                  const prime_exponents_view &e,
+                  const prime_exponents_view &f,
+                  int num_blocks) noexcept {
+      block_used = num_blocks;
+      for(int i = 0; i < block_used; ++i){
+        data()[i] = -a.data()[i] - b.data()[i] - c.data()[i] - d.data()[i] - e.data()[i] - f.data()[i];
+      }
+    }
+
+    [[nodiscard]] mwi::big_int<> split_sqrt_add(prime_exponents_view &add, const PrimeTable &prime_table){
+      mwi::big_int big_sqrt{1};
+      int num_blocks = std::max(block_used, add.block_used);
+      expand_blocks(num_blocks);
+      add.expand_blocks(num_blocks);
+
+      for(int i = 0; i < num_blocks; ++i){
+        exp_t sqrt_fpf = data()[i] & 1;
+        data()[i] += sqrt_fpf;
+        data()[i] /= 2;
+        data()[i] += add.data()[i];
+        if(sqrt_fpf){
+          big_sqrt *= prime_table.prime_list[i];
+        }
+      }
+      return big_sqrt;
+    }
   };
 
   class FactorPool{
@@ -73,12 +216,12 @@ namespace wigcpp::internal::global {
     aligned_vector<std::byte> buffer;
 
   public:
-    FactorPool(int max_factorial, int num_of_primes): aligned_block_bytes{}, buffer{}{
-      const std::size_t bytes_of_prime_exponents = sizeof(int) + sizeof(exp_t) * num_of_primes;
+    FactorPool(const PrimeTable &prime_table): aligned_block_bytes{}, buffer{}{
+      const std::size_t bytes_of_prime_exponents = sizeof(int) + sizeof(exp_t) * prime_table.num_primes;
       const std::size_t aligned_bytes = (bytes_of_prime_exponents + 63) & ~static_cast<std::size_t>(63);
       aligned_block_bytes = aligned_bytes;
-      buffer.resize((max_factorial + 1) * aligned_bytes, std::byte{0});
-      for(std::size_t i = 0; i <= max_factorial; ++i){
+      buffer.resize((prime_table.max_factorial + 1) * aligned_bytes, std::byte{0});
+      for(std::size_t i = 0; i <= prime_table.max_factorial; ++i){
         new (buffer.raw_pointer() + i * aligned_bytes) prime_exponents_view();
       }
     }
@@ -104,15 +247,17 @@ namespace wigcpp::internal::global {
   };
 
   class GlobalFactorialPool{
+  public:
+    PrimeTable prime_table;
+
+  private:
     template <typename T>
     using vector = container::vector<T>;
 
-    int max_factorial;
-    PrimeTable prime_table;
     FactorPool num_pool;
     FactorPool factorial_pool;
 
-    void fill_num_pool(int max_factorial) noexcept{
+    void fill_num_pool(const PrimeTable &prime_table) noexcept{
       const auto &prime_list = prime_table.prime_list;
       const int block_size = num_pool.block_size();
       uint64_t cur = 1;
@@ -121,7 +266,7 @@ namespace wigcpp::internal::global {
       while(!enum_done){
         int p = 0;
         while(true){
-          if(cur * prime_list[p] <= static_cast<uint64_t>(max_factorial)){
+          if(cur * prime_list[p] <= static_cast<uint64_t>(prime_table.max_factorial)){
             ++num_pool[0][p];
             cur *= prime_list[p];
             break;
@@ -146,12 +291,12 @@ namespace wigcpp::internal::global {
       std::fill(num_pool[0].data(), num_pool[0].data() + block_size, 0);
     }
 
-    void fill_factorial_pool(int max_factorial) noexcept {
+    void fill_factorial_pool(int max_factorial, uint32_t num_primes) noexcept {
       for(std::size_t i = 1; i <= max_factorial; ++i){
         exp_t *add = num_pool[i].data();
         exp_t *src = factorial_pool[i - 1].data();
         exp_t *dest = factorial_pool[i].data();
-        for(std::size_t p = 0; p < prime_table.num_primes; ++p){
+        for(std::size_t p = 0; p < num_primes; ++p){
           dest[p] = src[p] + add[p];
         }
 
@@ -161,13 +306,13 @@ namespace wigcpp::internal::global {
 
 
   public:
-    GlobalFactorialPool(int max_factorial) noexcept: max_factorial(max_factorial), prime_table(max_factorial), 
-        num_pool(max_factorial, prime_table.num_primes), 
-        factorial_pool(max_factorial, prime_table.num_primes){
-          fill_num_pool(max_factorial);
-          fill_factorial_pool(max_factorial);
+    GlobalFactorialPool(int max_factorial) noexcept: prime_table(max_factorial),
+        num_pool(prime_table), factorial_pool(prime_table)
+        {
+          fill_num_pool(prime_table);
+          fill_factorial_pool(max_factorial, prime_table.num_primes);
         }
-    
+
     GlobalFactorialPool() = delete;
     GlobalFactorialPool(const GlobalFactorialPool &) = delete;
     GlobalFactorialPool(GlobalFactorialPool &&) = delete;
@@ -182,13 +327,9 @@ namespace wigcpp::internal::global {
       return num_pool[n];
     }
 
-    const int factorial_capacity() const noexcept {
-      return max_factorial;
-    }
-
     #ifdef DEBUG_PRINT
     void print_factorial_pool() const  {
-      for(std::size_t i = 0; i <= max_factorial; ++i){
+      for(std::size_t i = 0; i <= prime_table.max_factorial; ++i){
         std::cout << "Factorial " << i << "! : ";
         for(std::size_t p = 0; p < prime_table.num_primes; ++p){
           std::cout << factorial_pool[i][p] << " ";
@@ -198,7 +339,7 @@ namespace wigcpp::internal::global {
     }
 
     void print_factorial_used() const  {
-      for(std::size_t i = 0; i <= max_factorial; ++i){
+      for(std::size_t i = 0; i <= prime_table.max_factorial; ++i){
         std::cout << "Fatorial " << i << "! used_block: " << factorial_pool[i].block_used << "\n";
       }
     }
