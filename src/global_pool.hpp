d5a707d6b72ed30708203316634ccdd87b6e8af7
Diketene <liuhaotian0406@163.com>
2025-08-05 13:22:20 +0800

    changed factor_pool.hpp to global_pool.hpp to make it more clearly

--- src/global_pool.hpp
+++ src/global_pool.hpp
@@ -3,7 +3,6 @@
 
 #include "definitions.hpp"
 #include "nothrow_allocator.hpp"
-#include "big_int.hpp"
 #include "vector.hpp"
 #include "error.hpp"
 #include <algorithm>
@@ -29,8 +28,9 @@ namespace wigcpp::internal::global {
     vector<uint32_t> prime_list;
     uint32_t num_primes;
     int max_factorial;
+    std::size_t aligned_bytes;
 
-    PrimeTable(int max_factorial) noexcept : prime_list{}, num_primes{0}, max_factorial(max_factorial){
+    PrimeTable(int max_factorial) noexcept : prime_list{}, num_primes{0}, max_factorial(max_factorial), aligned_bytes(0){
       vector<char> is_prime(max_factorial + 1, true);
       for(int i = 2; i * i < max_factorial; ++i){
         if(is_prime[i]){
@@ -46,6 +46,9 @@ namespace wigcpp::internal::global {
           prime_list.push_back(i);
         }
       }
+      const std::size_t bytes_of_prime_exponents = sizeof(int) + sizeof(exp_t) * num_primes;
+      const std::size_t aligned_block_bytes = (bytes_of_prime_exponents + 63) & ~static_cast<std::size_t>(63);
+      aligned_bytes = aligned_block_bytes;
     }
 
     PrimeTable() = delete;
@@ -55,11 +58,11 @@ namespace wigcpp::internal::global {
     int block_used;
 
     exp_t *data() noexcept{
-      return reinterpret_cast<exp_t*>(this + 1);
+      return reinterpret_cast<exp_t*>(this) + 1;
     }
 
     const exp_t *data() const noexcept {
-      return reinterpret_cast<const exp_t*>(this + 1);
+      return reinterpret_cast<const exp_t*>(this) + 1;
     }
 
     exp_t &operator[](std::size_t i) noexcept {
@@ -70,7 +73,8 @@ namespace wigcpp::internal::global {
       return data()[i];
     }
 
-    void set_zero() noexcept {
+    void set_zero(int num_blocks) noexcept {
+      block_used = num_blocks;
       std::memset(data(), 0, sizeof(exp_t) * block_used);
     }
 
@@ -84,6 +88,7 @@ namespace wigcpp::internal::global {
     } 
 
     void set_max(int num_blocks) noexcept {
+      block_used = num_blocks;
       std::fill(data(), data() + num_blocks, def::prime::max_exp);
     }
 
@@ -118,7 +123,7 @@ namespace wigcpp::internal::global {
     void expand_sub(const prime_exponents_view &other) noexcept{
       expand_blocks(other.block_used);
       for(int i = 0; i < block_used; ++i){
-        data()[i] -= other.data()[i];
+        data()[i] -= other[i];
       }
     }
 
@@ -191,9 +196,17 @@ namespace wigcpp::internal::global {
       }
     }
 
-    [[nodiscard]] mwi::big_int<> split_sqrt_add(prime_exponents_view &add);
-    
   };
+
+  #ifdef DEBUG_PRINT
+  inline void dump_fpf(const char *name, const prime_exponents_view &fpf){
+    std::printf("%20s:", name);
+    for(int j = 0; j < fpf.block_used; j++){
+      std::printf("%d ", fpf[j]);
+    }
+    std::printf("\n");
+  }
+  #endif
 
   class FactorPool{
     template <typename T>
@@ -203,13 +216,10 @@ namespace wigcpp::internal::global {
     aligned_vector<std::byte> buffer;
 
   public:
-    FactorPool(const PrimeTable &prime_table): aligned_block_bytes{}, buffer{}{
-      const std::size_t bytes_of_prime_exponents = sizeof(int) + sizeof(exp_t) * prime_table.num_primes;
-      const std::size_t aligned_bytes = (bytes_of_prime_exponents + 63) & ~static_cast<std::size_t>(63);
-      aligned_block_bytes = aligned_bytes;
-      buffer.resize((prime_table.max_factorial + 1) * aligned_bytes, std::byte{0});
+    FactorPool(const PrimeTable &prime_table): aligned_block_bytes{prime_table.aligned_bytes}, buffer{}{
+      buffer.resize((prime_table.max_factorial + 1) * aligned_block_bytes, std::byte{0});
       for(std::size_t i = 0; i <= prime_table.max_factorial; ++i){
-        new (buffer.raw_pointer() + i * aligned_bytes) prime_exponents_view();
+        new (buffer.raw_pointer() + i * aligned_block_bytes) prime_exponents_view();
       }
     }
 
@@ -225,6 +235,10 @@ namespace wigcpp::internal::global {
       return (aligned_block_bytes - sizeof(int)) / sizeof(exp_t);
     }
 
+    std::size_t aligned_bytes() const noexcept {
+      return aligned_block_bytes;
+    }
+
     FactorPool(const FactorPool &) = delete;
     FactorPool &operator= (const FactorPool &) = delete;
     FactorPool(FactorPool &&) = delete;
@@ -236,6 +250,8 @@ namespace wigcpp::internal::global {
   class GlobalFactorialPool{
   public:
     PrimeTable prime_table;
+    int max_two_j;
+    int wigner_type;
 
   private:
     template <typename T>
@@ -293,11 +309,12 @@ namespace wigcpp::internal::global {
 
 
   public:
-    GlobalFactorialPool(int max_factorial) noexcept: prime_table(max_factorial),
+    GlobalFactorialPool(int max_two_j, int wigner_type) noexcept: max_two_j(max_two_j), wigner_type(wigner_type), 
+        prime_table(((wigner_type / 3 + 2) * (max_two_j / 2)) + 1),
         num_pool(prime_table), factorial_pool(prime_table)
         {
           fill_num_pool(prime_table);
-          fill_factorial_pool(max_factorial, prime_table.num_primes);
+          fill_factorial_pool(max_two_j * wigner_type / 2 + 1, prime_table.num_primes);
         }
 
     GlobalFactorialPool() = delete;
@@ -306,12 +323,22 @@ namespace wigcpp::internal::global {
     GlobalFactorialPool &operator= (const GlobalFactorialPool &) = delete;
     GlobalFactorialPool &operator= (GlobalFactorialPool &&) = delete;
 
-    const prime_exponents_view &factorial_prime_factors(std::size_t n) const noexcept {
+    /* We use a different way to provide methods that used in querying the global factor pool.
+    ** We provide a [] operator overload for the factorial pool, 
+    ** and a prime_factor() method for the prime factor pool, 
+    ** because the factorial pool is used more frequently than the prime factor pool.
+    */
+
+    const prime_exponents_view &operator[] (std::size_t n) const noexcept {
       return factorial_pool[n];
     }
 
-    const prime_exponents_view &number_prime_factors(std::size_t n) const noexcept {
+    const prime_exponents_view &prime_factor(std::size_t n) const noexcept {
       return num_pool[n];
+    }
+
+    const std::size_t aligned_bytes() const noexcept {
+      return factorial_pool.aligned_bytes();
     }
 
     #ifdef DEBUG_PRINT
@@ -342,8 +369,9 @@ namespace wigcpp::internal::global {
     PoolManager() = delete;
     ~PoolManager() = delete;
   public:
-    static void init(int max_factorial) noexcept{
-      if(max_factorial < 2){
+    static void init(int max_two_j, int wigner_type) noexcept{
+      int max_factorial = (wigner_type / 3 + 2) * (max_two_j / 2) + 1;
+      if( max_factorial < 2){
         max_factorial = 2;
       }
 
@@ -357,7 +385,7 @@ namespace wigcpp::internal::global {
         return;
       }
 
-      ptr = std::make_unique<GlobalFactorialPool>(max_factorial);
+      ptr = std::make_unique<GlobalFactorialPool>(max_two_j, wigner_type);
       is_initialized.store(true, std::memory_order_release);
     }
 
@@ -369,25 +397,6 @@ namespace wigcpp::internal::global {
       return *ptr;
     }
   };
-
-  [[nodiscard]] inline mwi::big_int<> prime_exponents_view::split_sqrt_add(prime_exponents_view &add){
-    const auto &prime_table = PoolManager::get().prime_table;
-    mwi::big_int big_sqrt{1};
-      int num_blocks = std::max(block_used, add.block_used);
-      expand_blocks(num_blocks);
-      add.expand_blocks(num_blocks);
-
-      for(int i = 0; i < num_blocks; ++i){
-        exp_t sqrt_fpf = data()[i] & 1;
-        data()[i] += sqrt_fpf;
-        data()[i] /= 2;
-        data()[i] += add.data()[i];
-        if(sqrt_fpf){
-          big_sqrt *= prime_table.prime_list[i];
-        }
-      }
-      return big_sqrt;
-    }
 
 }
 
