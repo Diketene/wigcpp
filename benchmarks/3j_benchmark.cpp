b2ecfb03e9296cf1542113f29a166f17d829c1cf
Diketene <liuhaotian0406@163.com>
2025-08-27 09:09:10 +0800

    add 3j symbol benchmark

---
+++ benchmarks/3j_benchmark.cpp
@@ -0,0 +1,14 @@
+#include "benchmark/benchmark.h"
+#include "wigcpp/wigcpp.hpp"
+
+static void BM_3j(benchmark::State& state) {
+  wigcpp::global_init(2 * 100, 9);
+  for(auto _ : state){
+    auto res = wigcpp::three_j(2 * 15, 2 * 30, 2 * 40, 2 * 2, 2 * 2, -2 * 4);
+    benchmark::DoNotOptimize(res);
+  }
+}
+
+BENCHMARK(BM_3j);
+
+BENCHMARK_MAIN();
