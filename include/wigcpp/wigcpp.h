e61f23b80b5c24e4793f70dcf17a250a00fc1c19
Diketene <liuhaotian0406@163.com>
2025-09-10 20:48:28 +0800

    added function calculates clebsh gordan coefficient

--- include/wigcpp/wigcpp.h
+++ include/wigcpp/wigcpp.h
@@ -14,6 +14,7 @@
 extern "C"{
 #endif
   void wigcpp_global_init(int max_two_j, int wigner_type);
+  double clebsh_gordan(int two_j1, int two_m1, int two_j2, int two_m2, int two_J, int two_M);
   double wig3jj(int two_j1, int two_j2, int two_j3, int two_m1, int two_m2, int two_m3);
   double wig6jj(int two_j1, int two_j2, int two_j3, int two_j4, int two_j5, int two_j6);
   double wig9jj(int two_j1, int two_j2, int two_j3, int two_j4, int two_j5, int two_j6, int two_j7, int two_j8, int two_j9);
