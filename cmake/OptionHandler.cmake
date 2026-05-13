366095126a6137c39eb8bb915364e74e46077b06
Diketene <liuhaotian0406@163.com>
2026-05-14 00:07:33 +0800

    refactored main CMakeList

---
+++ cmake/OptionHandler.cmake
@@ -0,0 +1,48 @@
+if(BUILD_SHARED_LIBS) 
+  set_target_properties(wigcpp
+    PROPERTIES
+      CXX_VISIBILITY_PRESET hidden
+      VISIBILITY_INLINES_HIDDEN ON
+  )
+endif()
+
+if(WIGCPP_BUILD_TEST)
+  enable_testing()
+  add_subdirectory(tests)
+else()
+  message(STATUS "Testing is disabled.") 
+endif()
+
+if(WIGCPP_BUILD_BENCHMARK)
+  add_subdirectory(benchmarks)
+endif()
+
+if(WIGCPP_BUILD_FORTRAN_INTERFACE)
+  include(CheckLanguage)
+  check_language(Fortran)
+  if(NOT CMAKE_Fortran_COMPILER)
+    message(STATUS "Fortran compiler not found, disabling Fortran interface.")
+  else()
+    enable_language(Fortran)
+    message(STATUS "Fortran support enabled(compiler: ${CMAKE_Fortran_COMPILER})")
+    add_library(fortran_interface OBJECT src/fortran/interface.f90)
+
+    set(CMAKE_Fortran_MODULE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/mod)
+    file(MAKE_DIRECTORY ${CMAKE_Fortran_MODULE_DIRECTORY})
+
+    set_target_properties(
+      fortran_interface 
+      PROPERTIES 
+      Fortran_MODULE_DIRECTORY 
+      ${CMAKE_Fortran_MODULE_DIRECTORY}
+    )
+
+    target_include_directories(fortran_interface PUBLIC ${CMAKE_Fortran_MODULE_DIRECTORY})
+    target_sources(wigcpp PRIVATE $<TARGET_OBJECTS:fortran_interface>)
+
+    install(
+      FILES "${CMAKE_Fortran_MODULE_DIRECTORY}/wigcpp.mod"
+      DESTINATION include
+    )
+  endif()
+endif()
