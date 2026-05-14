60f053e6aa22464aa26d090df52b5e7fe59b1e16
Diketene <liuhaotian0406@163.com>
2026-05-14 21:29:19 +0800

    optimized build CML for Fortran interface

--- cmake/OptionHandler.cmake
+++ cmake/OptionHandler.cmake
@@ -27,22 +27,23 @@ if(WIGCPP_BUILD_FORTRAN_INTERFACE)
     message(STATUS "Fortran support enabled(compiler: ${CMAKE_Fortran_COMPILER})")
     add_library(fortran_interface OBJECT src/fortran/interface.f90)
 
-    set(CMAKE_Fortran_MODULE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/mod)
-    file(MAKE_DIRECTORY ${CMAKE_Fortran_MODULE_DIRECTORY})
+    set(_fortran_mod_dir ${CMAKE_CURRENT_BINARY_DIR}/mod)
+    file(MAKE_DIRECTORY ${_fortran_mod_dir})
 
     set_target_properties(
       fortran_interface 
       PROPERTIES 
       Fortran_MODULE_DIRECTORY 
-      ${CMAKE_Fortran_MODULE_DIRECTORY}
+      ${_fortran_mod_dir}
     )
 
-    target_include_directories(fortran_interface PUBLIC ${CMAKE_Fortran_MODULE_DIRECTORY})
+    target_include_directories(fortran_interface PUBLIC ${_fortran_mod_dir})
     target_sources(wigcpp PRIVATE $<TARGET_OBJECTS:fortran_interface>)
 
     install(
-      FILES "${CMAKE_Fortran_MODULE_DIRECTORY}/wigcpp.mod"
+      DIRECTORY "${_fortran_mod_dir}/"
       DESTINATION include
+      FILES_MATCHING PATTERN "*.mod"
     )
   endif()
 endif()
